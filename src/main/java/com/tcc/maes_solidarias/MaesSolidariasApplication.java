764fb1b8aad2a07c2fd7008bc6e5e17afaa93966
Eduarda Ferreira <eduarda.ilegra@terceiros.zenvia.com>
2019-10-25 16:38:30 -0300

    alterando front

---
+++ src/main/java/com/tcc/maes_solidarias/MaesSolidariasApplication.java
@@ -0,0 +1,13 @@
+package com.tcc.maes_solidarias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaesSolidariasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaesSolidariasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tcc/maes_solidarias/MaesSolidariasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcc.maes_solidarias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaesSolidariasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
